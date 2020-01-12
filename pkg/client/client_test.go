592071aba05d4b63c6f4db700f7c88a72c5cad69
Kazuhiro Kawachi <kawachi@example.com>
2020-01-12 23:16:20 +0900

    Add unit test client.go

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestClient(t *testing.T) {
+func TestClient_Get01(t *testing.T) {
 
 	cfg, err := NewConfig()
 	if err != nil {
@@ -21,6 +21,150 @@ func TestClient(t *testing.T) {
 	}
 	s := fmt.Sprintf("%s", err)
 	if s != "Error: StatusCode:400 Code:6 Message: Undefined resource. /api/v2/ MoreInfo:" {
+		fmt.Println(err)
+	}
+}
+
+func TestClient_Get02(t *testing.T) {
+
+	cfg, err := NewConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	api := "api/v2/issues"
+	apiKey := "xxxxxxxxxxxxxxx" //Error
+
+	cli := NewClient(cfg.Space, apiKey)
+	_, err = cli.Get(api, nil)
+	if err == nil {
+		t.Fatal(err)
+	}
+	s := fmt.Sprintf("%s", err)
+	if s != "Error: StatusCode:401 Code:11 Message: Authentication failure. MoreInfo:" {
+		fmt.Println(err)
+	}
+}
+
+func TestClient_Post01(t *testing.T) {
+
+	cfg, err := NewConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	api := "api/v2" //Error
+
+	cli := NewClient(cfg.Space, cfg.APIKey)
+	_, err = cli.Post(api, nil)
+	if err == nil {
+		t.Fatal(err)
+	}
+	s := fmt.Sprintf("%s", err)
+	if s != "Error: StatusCode:400 Code:6 Message: Undefined resource. /api/v2/ MoreInfo:" {
+		fmt.Println(err)
+	}
+}
+
+func TestClient_Post02(t *testing.T) {
+
+	cfg, err := NewConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	api := "api/v2/issues"
+	apiKey := "xxxxxxxxxxxxxxx" //Error
+
+	cli := NewClient(cfg.Space, apiKey)
+	_, err = cli.Post(api, nil)
+	if err == nil {
+		t.Fatal(err)
+	}
+	s := fmt.Sprintf("%s", err)
+	if s != "Error: StatusCode:401 Code:11 Message: Authentication failure. MoreInfo:" {
+		fmt.Println(err)
+	}
+}
+
+func TestClient_Patch01(t *testing.T) {
+
+	cfg, err := NewConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	api := "api/v2" //Error
+
+	cli := NewClient(cfg.Space, cfg.APIKey)
+	_, err = cli.Patch(api, nil)
+	if err == nil {
+		t.Fatal(err)
+	}
+	s := fmt.Sprintf("%s", err)
+	if s != "Error: StatusCode:400 Code:6 Message: Undefined resource. /api/v2/ MoreInfo:" {
+		fmt.Println(err)
+	}
+}
+
+func TestClient_Patch02(t *testing.T) {
+
+	cfg, err := NewConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	api := "api/v2/issues/0"
+	apiKey := "xxxxxxxxxxxxxxx" //Error
+
+	cli := NewClient(cfg.Space, apiKey)
+	_, err = cli.Patch(api, nil)
+	if err == nil {
+		t.Fatal(err)
+	}
+	s := fmt.Sprintf("%s", err)
+	if s != "Error: StatusCode:401 Code:11 Message: Authentication failure. MoreInfo:" {
+		fmt.Println(err)
+	}
+}
+
+func TestClient_Delete01(t *testing.T) {
+
+	cfg, err := NewConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	api := "api/v2" //Error
+
+	cli := NewClient(cfg.Space, cfg.APIKey)
+	_, err = cli.Delete(api, nil)
+	if err == nil {
+		t.Fatal(err)
+	}
+	s := fmt.Sprintf("%s", err)
+	if s != "Error: StatusCode:400 Code:6 Message: Undefined resource. /api/v2/ MoreInfo:" {
+		fmt.Println(err)
+	}
+}
+
+func TestClient_Delete02(t *testing.T) {
+
+	cfg, err := NewConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	api := "api/v2/issues/0"
+	apiKey := "xxxxxxxxxxxxxxx" //Error
+
+	cli := NewClient(cfg.Space, apiKey)
+	_, err = cli.Delete(api, nil)
+	if err == nil {
+		t.Fatal(err)
+	}
+	s := fmt.Sprintf("%s", err)
+	if s != "Error: StatusCode:401 Code:11 Message: Authentication failure. MoreInfo:" {
 		fmt.Println(err)
 	}
 }
